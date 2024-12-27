3ce77dec3464714ca53791c097bce34d31f2a8d1
Clary Nguyen <thuytiennguyen140103@gmail.com>
2024-12-27 10:31:24 +0700

    Implement Project model to store projects

---
+++ back-end/src/model/ProjectModel.js
@@ -0,0 +1,50 @@
+import { Sequelize } from 'sequelize'
+import { DataTypes } from 'sequelize'
+
+const sequelize = new Sequelize({
+    dialect: "sqlite",
+    storage: "database.sqlite",
+});
+
+const Project = sequelize.define('Project', {
+    id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+    },
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        defaultValue: 'Project Title',
+    },
+    description: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        defaultValue: 'There is no description for this project.',
+    },
+    tech: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    time: {
+        type: DataTypes.DATEONLY,
+        allowNull: false,
+    },
+    image: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    }
+});
+
+class _ProjectModel {
+    constructor(){}
+
+    async init(fresh = false) {
+        await sequelize.authenticate();
+        await sequelize.sync({ force: true });
+
+        if(fresh) {
+            // init with default values
+        }
+    }
+}
