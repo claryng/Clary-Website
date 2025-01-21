b3dfda90869d5433042044a727b9981d328eef48
Clary Nguyen <thuytiennguyen140103@gmail.com>
2025-01-21 17:41:21 +0700

    Move files to docs

--- front-end/src/main.js
+++
@@ -1,7 +0,0 @@
-import { AppControllerComponent } from "./components/AppControllerComponent/AppControllerComponent.js";
-
-const appController = new AppControllerComponent();
-
-// Render the component in the #app container
-const appContainer = document.getElementById('app');
-appContainer.appendChild(appController.render());/* main.js */
